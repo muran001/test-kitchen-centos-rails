305aea02b1f536d4bf3f85a7fc31d853cc6deeff
muran001 <macmuran001@gmail.com>
2014-10-05 15:21:36 +0900

    first init ( berks cookbook rails-cookbook )

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'rails-cookbook'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures rails-cookbook'
+long_description 'Installs/Configures rails-cookbook'
+version          '0.1.0'
+
